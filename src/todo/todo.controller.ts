b7f7d046a2e33dff6c50a8c0e58800cc7d06524a
chihanainoue <smarprise.chihana.inoue@gmail.com>
2024-02-08 21:13:03 +0900

    データベースの接続

---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('todo')
+export class TodoController {
+  @Get('list')
+  getList() {
+    return [
+      {
+        title: '牛乳を買いに行く',
+        due_on: '2022-12-24',
+        done: false,
+      },
+    ];
+  }
+}
